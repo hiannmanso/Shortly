53649691008400c8f3e4e10eaea62ddec82a1bce
Hiann <hiannmanso@email.com>
2022-06-08 01:43:46 -0300

    heroku settings

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import chalk from 'chalk'
 import dotenv from 'dotenv'
-import categoriesRouter from './Routes/categoriesRouter.js'
-import gamesRouter from './Routes/gamesRouter.js'
-import clientsRouter from './Routes/clientsRouter.js'
-import rentalsRouter from './Routes/rentalsRouter.js'
 
 dotenv.config()
 
@@ -13,14 +9,10 @@ const server = express()
 
 server.use(cors())
 server.use(express.json())
-server.get(
-	'/',
-	(req,
-	(res) => {
-		res.send('teste')
-		console.log('teste')
-	})
-)
+server.get('/', (req, res) => {
+	res.send('teste')
+	console.log('teste')
+})
 
 server.listen(process.env.PORT, () => {
 	console.log(chalk.bold.green(`Backend up on port:${process.env.PORT}`))
