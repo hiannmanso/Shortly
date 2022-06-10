fc573942d842c53f3e6019988fa73f47909e0f3c
Hiann <hiannmanso@email.com>
2022-06-10 14:51:20 -0300

    heroku again

--- Middlewares/validateSchema.js
+++ Middlewares/validateSchema.js
@@ -1,5 +1,5 @@
 export async function validateSchema(req, res, next, schema) {
 	const validation = schema.validate(req.body)
-	if (validation.error) return res.status(422).send(error)
+	if (validation.error) return res.status(422).send(validation.error)
 	next()
 }
