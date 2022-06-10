6967781595aafe67176fadeccac4399530b9860e
Hiann <hiannmanso@email.com>
2022-06-10 14:45:58 -0300

    heroku again

--- schemas/signupSchema/signUpSchema.js
+++ schemas/signupSchema/signUpSchema.js
@@ -1,14 +1,14 @@
-import joi from 'joi'
+// import joi from 'joi'
 
-const signUpSchema = joi.object({
-	name: joi
-		.string()
-		.min(3)
-		.pattern(/^[a-zA-ZãÃÇ-Üá-ú ]*$/)
-		.required(),
-	email: joi.string().email().required(),
-	password: joi.string().min(5).required(),
-	confirmPassword: joi.string().valid(joi.ref('password')).required(),
-})
+// const signUpSchema = joi.object({
+// 	name: joi
+// 		.string()
+// 		.min(3)
+// 		.pattern(/^[a-zA-ZãÃÇ-Üá-ú ]*$/)
+// 		.required(),
+// 	email: joi.string().email().required(),
+// 	password: joi.string().min(5).required(),
+// 	confirmPassword: joi.string().valid(joi.ref('password')).required(),
+// })
 
-export default signUpSchema
+// export default signUpSchema
