c743bf2be2e998333690d827b5dc5686f6ddab03
Hiann <hiannmanso@email.com>
2022-06-10 14:08:57 -0300

    heroku again

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -2,15 +2,11 @@ import express from 'express'
 import { signInPOST, signUpPOST } from '../Controllers/authControllers.js'
 import { validateSchema } from '../Middlewares/validateSchema.js'
 // import signInSchema from '../Schemas/signInSchema/signInSchema.js'
-import signUpSchema from '../Schemas/signupSchema/signUpSchema.js'
+// import signUpSchema from '../Schemas/signupSchema/signUpSchema.js'
 
 const authRouter = express.Router()
 
-authRouter.post(
-	'/signup',
-	(req, res, next) => validateSchema(req, res, next, signUpSchema),
-	signUpPOST
-)
+authRouter.post('/signup', signUpPOST)
 authRouter.post('/signin', signInPOST)
 
 export default authRouter
