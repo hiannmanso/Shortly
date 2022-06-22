ff7d7f9c596e258b053267a9203d86ca59f6e191
pedro imbrech <elprogramadorpedro@gmail.com>
2022-06-22 17:29:28 -0300

    rev

--- Middlewares/validateSchema.js
+++
@@ -1,5 +0,0 @@
-export async function validateSchema(req, res, next, schema) {
-	const validation = schema.validate(req.body)
-	if (validation.error) return res.status(422).send(validation.error)
-	next()
-}
